0b2e122cee6485753bed0aab590013195eb6dc01
Keith <Keeth@DESKTOP-2IBSRIO>
2024-03-18 23:51:00 +0000

    LinkedList DataStructure initialize, Car reg created with yyDxnnnn where the yy is a 14 to 24, D refers to Dublin, x is either 1 or 2 which relates to the car being registered in the first or second half of the year; and the nnnn are any 4 integers. Including the reg we also included car make and model, colour, number of previous owners which cannot be more than 3. A value of 0 indicates that this is a new car. A boolean value to indicate if a car is reserved or not, A car is reserved if a customer has placed a deposit on the car with the intention to buy it later. Finally a reserVe amout, which is a deposit that a customer pays in lieu of buying a car. This amount will be 0 if the car is unreserved.

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,14 +2,58 @@
 //C22415964
 //14/03/24 Start date for Project
 #define _CRT_SECURE_NO_WARNINGS 1 // No warnings
-// Include necessary header files
 #include <stdio.h>
+#include <stdlib.h> // For dynamic memory allocation
+#include <string.h> // For string manipulation functions
 
-// Main function
+// Structure to represent a car
+struct Car {
+    char registration[10]; // Car registration number
+    char make[50]; // Make of the car
+    char model[50]; // Model of the car
+    char color[20]; // Color of the car
+    int year; // Year of the car
+    int num_owners; // Number of previous owners (0 to 3)
+    int reserved; // Boolean value indicating if the car is reserved (1 for reserved, 0 for not reserved)
+    float reserve_amount; // Reserve amount (deposit paid by the customer)
+    struct Car* next; // Pointer to the next car node in the linked list
+};
+
+// Function to validate car registration format
+int isValidRegistration(const char* registration) {
+    // Check if registration has 8 characters
+    if (strlen(registration) != 8)
+        return 0;
+
+    // Validate year (14 to 24)
+    int year = (registration[0] - '0') * 10 + (registration[1] - '0');
+    if (year < 14 || year > 24)
+        return 0;
+
+    // Validate Dublin identifier
+    if (registration[2] != 'D')
+        return 0;
+
+    // Validate half-year indicator (1 or 2)
+    if (registration[3] != '1' && registration[3] != '2')
+        return 0;
+
+    // Validate last 4 digits
+    for (int i = 4; i < 8; i++) {
+        if (!isdigit(registration[i]))
+            return 0;
+    }
+
+    // Registration format is valid
+    return 1;
+}
+
 int main() {
-    int choice; // Variable to store user's choice
+    int choice;
 
-    // Start of do-while loop to display menu and get user input
+    // Initialize the head of the linked list
+    struct Car* head = NULL;
+
     do {
         // Displaying the menu
         printf("Car Showroom Management System\n");
@@ -30,6 +74,16 @@ int main() {
         case 1:
             printf("Adding a new car to the showroom...\n");
             
+            printf("Functionality to add a new car will be implemented here.\n");
+
+            
+            char registration[10];
+            printf("Enter car registration: ");
+            scanf("%s", registration);
+            if (isValidRegistration(registration))
+                printf("Valid registration!\n");
+            else
+                printf("Invalid registration format!\n");
             break;
         case 2:
             printf("Selling a car from the showroom...\n");
@@ -45,11 +99,11 @@ int main() {
             break;
         case 5:
             printf("Viewing a specific car in the showroom...\n");
-            
+           
             break;
         case 6:
             printf("Your custom option...\n");
-            
+           
             break;
         case 7:
             printf("Exiting the system. Goodbye!\n");
@@ -57,9 +111,8 @@ int main() {
         default:
             printf("Invalid choice! Please enter a number between 1 and 7.\n");
         }
-    } while (choice != 7); // End of do-while loop, exit loop if choice is 7 (exit)
+    } while (choice != 7);
 
-    return 0; // Return 0 to indicate successful execution
+    return 0;
 }
-//This comment is to check if pushing to remote repo is working as intended 
-//This is just one more test just to see if it is finally working 
+}
